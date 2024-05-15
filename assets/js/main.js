47ae5f0d805660cbdc8101a0ab0d5d43e1fd2470
Mei Yang <e2296236@cmaisonneuve.qc.ca>
2024-05-15 14:14:27 -0400

    Adjuster la version tablette et ajouter animation au curseur et aux boutons

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+let cursor1 = document.querySelector('#cursor-1'),
+    cursor2 = document.querySelector('#cursor-2');
+
+console.log(cursor1);
+
+
+document.body.onpointermove = event => 
+{
+    const { clientX, clientY } = event;
+    
+        cursor1.animate({
+            left: `${clientX}px`,
+            top: `${clientY}px`
+        
+        }, {duration: 1000, fill: "forwards"})
+    
+        cursor2.animate({
+            left: `${clientX}px`,
+            top: `${clientY}px`
+        
+        }, {duration: 100, fill: "forwards"})
+    
+}
